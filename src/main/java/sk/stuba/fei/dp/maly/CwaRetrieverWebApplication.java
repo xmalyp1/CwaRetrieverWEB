da5a71a50b7311b4d3586de5a6b32e83d29f9725
Patrik Malý <maly.patrik1@gmail.com>
2017-04-02 11:47:10 +0200

    openshift-data

--- src/main/java/sk/stuba/fei/dp/maly/CwaRetrieverWebApplication.java
+++ src/main/java/sk/stuba/fei/dp/maly/CwaRetrieverWebApplication.java
@@ -3,7 +3,7 @@ package sk.stuba.fei.dp.maly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
